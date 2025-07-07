0d3c8fb391dbe3e1cda6fd81fc532b78a9a1275c
NYAINA_FITIAVANA <nfitahiantsoa@ingenosya.mg>
2025-07-07 16:27:40 +0300

    Corrected error server

--- src/app/builder/[uuid]/page.tsx
+++ src/app/builder/[uuid]/page.tsx
@@ -1,5 +1,7 @@
 import BuilderPageClient from "./BuilderPageClient";
 
-export default function BuilderPage({ params }: { params: { uuid: string } }) {
-  return <BuilderPageClient uuid={params.uuid} />;
+export default async function BuilderPage({ params }: { params: Promise<{ uuid: string }> }) {
+  const resolvedParams = await params;
+
+  return <BuilderPageClient uuid={resolvedParams.uuid} />;
 }
